5586cc6d7fedc385616f628162e3832637320564
Олег <oleg.kyznetzov@mail.ru>
2024-09-06 18:56:29 +0400

    added new interface for wordbook-preview

--- src/main/java/com/kuznetsov/dictionarypc/controller/WordbookPreviewController.java
+++ src/main/java/com/kuznetsov/dictionarypc/controller/WordbookPreviewController.java
@@ -2,13 +2,21 @@ package com.kuznetsov.dictionarypc.controller;
 
 import com.kuznetsov.dictionarypc.entity.Wordbook;
 import com.kuznetsov.dictionarypc.utils.WindowsManager;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 public class WordbookPreviewController {
     @FXML
     public Button testButton;
+    @FXML
+    public ChoiceBox<String> testType;
+    @FXML
+    public ChoiceBox<String> wordType;
     @FXML
     private Label nameLabel;
     @FXML
@@ -16,7 +24,15 @@ public class WordbookPreviewController {
     private Wordbook wordbook;
 
     public void initialize() {
+        ObservableList<String> testTypes =
+                FXCollections.<String>observableArrayList("Write first", "Write second");
+        testType.setItems(testTypes);
+        testType.setValue("Write second");
 
+        ObservableList<String> wordTypes =
+                FXCollections.<String>observableArrayList("All words", "Wrong", "New");
+        wordType.setItems(wordTypes);
+        wordType.setValue("All words");
     }
 
     public void setWordbook(Wordbook wordbook) {
