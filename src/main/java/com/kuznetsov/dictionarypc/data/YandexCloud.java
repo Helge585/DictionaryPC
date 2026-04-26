79ad26574489abccec997e8c7f16f6d65f471dd1
Олег <oleg.kyznetzov@mail.ru>
2026-04-26 22:00:48 +0300

    Test Yandex Cloud API - downloading and uploading file

--- src/main/java/com/kuznetsov/dictionarypc/data/YandexCloud.java
+++ src/main/java/com/kuznetsov/dictionarypc/data/YandexCloud.java
@@ -3,6 +3,9 @@ package com.kuznetsov.dictionarypc.data;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -34,8 +37,53 @@ public class YandexCloud {
     }
 
     private static void sandbox() {
-        sandboxGetFile();
+//        sandboxGetFile();
+        sandboxSaveFile();
+    }
 
+    private static void sandboxSaveFile() {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpGet getRequest = new HttpGet(
+                    "https://cloud-api.yandex.net/v1/disk/resources/upload?path=" + testPath + "&overwrite=true"
+            );
+            getRequest.setHeader("Content-Type", "application/json; charset=UTF-8");
+            getRequest.setHeader("Authorization", "OAuth " + OAuthToken);
+
+            try (CloseableHttpResponse response = client.execute(getRequest)) {
+                System.out.println("Result: " + response.getStatusLine());
+
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String jsonResponse = new String(entity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+
+                    String uploadUrl = extractHrefFromJson(jsonResponse);
+                    System.out.println("Upload URL: " + uploadUrl);
+
+                    String fileContent = "It's working!!! Это работает!!!";
+
+                    byte[] contentBytes = fileContent.getBytes(StandardCharsets.UTF_8);
+
+                    HttpPut putRequest = new HttpPut(uploadUrl);
+                    putRequest.setHeader("Content-Type", "text/plain; charset=UTF-8");
+//                    putRequest.setHeader("Content-Length", String.valueOf(contentBytes.length));
+                    putRequest.setEntity(new ByteArrayEntity(contentBytes, ContentType.TEXT_PLAIN));
+
+                    try (CloseableHttpResponse putResponse = client.execute(putRequest)) {
+                        System.out.println("!!!!");
+                        int statusCode = putResponse.getStatusLine().getStatusCode();
+                        if (statusCode == 201 || statusCode == 200) {
+                            System.out.println(statusCode + ": File was saved");
+                        } else {
+                            System.err.println(statusCode + ": Error. File wasn't saved");
+                        }
+                    }
+                } else {
+                    System.out.println("Failed to get save link: " + response.getStatusLine());
+                }
+            }
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
     private static void sandboxGetFile() {
@@ -48,13 +96,58 @@ public class YandexCloud {
 
             try (CloseableHttpResponse response = client.execute(getRequest)) {
                 System.out.println("Result: " + response.getStatusLine());
-                HttpEntity entity = response.getEntity();
+//                HttpEntity entity = response.getEntity();
 
-                System.out.println(new String(entity.getContent().readAllBytes(), StandardCharsets.UTF_8));
+//                System.out.println(new String(entity.getContent().readAllBytes(), StandardCharsets.UTF_8));
+
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String jsonResponse = new String(entity.getContent().readAllBytes(), StandardCharsets.UTF_8);
+
+                    String downloadUrl = extractHrefFromJson(jsonResponse);
+                    System.out.println(downloadUrl);
+
+                    HttpGet downloadRequest = new HttpGet(downloadUrl);
+
+                    try (CloseableHttpResponse downloadResponse = client.execute(downloadRequest)){
+                        if (downloadResponse.getStatusLine().getStatusCode() == 200) {
+                            HttpEntity downloadEntity = downloadResponse.getEntity();
+                            String fileContent = new String(
+                                    downloadEntity.getContent().readAllBytes(),
+                                    StandardCharsets.UTF_8
+                            );
+                            System.out.println("fileContent:\n" + fileContent);
+                        } else {
+                            System.err.println("Failed to download file: " + downloadResponse.getStatusLine());
+                        }
+                    }
+                } else {
+                    System.out.println("Failed to get download link: " + response.getStatusLine());
+                }
             }
 
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    private static String extractHrefFromJson(String json) {
+        //TODO: использовать Jackson или Gson
+        //В документации API описаны класы Link и Link-upload
+
+        String pattern = "\"href\":\"";
+
+        int start = json.indexOf(pattern);
+        if (start == -1) {
+            throw new RuntimeException("No 'href' field");
+        }
+        start += pattern.length();
+
+        int end = json.indexOf("\"", start);
+        if (end == -1) {
+            throw new RuntimeException("Incorrect 'href' field");
+        }
+
+        return json.substring(start, end);
     }
 }
